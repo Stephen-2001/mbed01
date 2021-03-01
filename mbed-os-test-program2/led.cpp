516de0561da1b7648d2788be75ce8bdfbe7d1bd7
Stephen-2001 <peers0212@gmail.com>
2021-02-28 19:29:38 -0800

    add led.cpp

---
+++ mbed-os-test-program2/led.cpp
@@ -0,0 +1,9 @@
+#include "mbed.h"
+
+void Led(DigitalOut &ledpin){
+    for(int i=0; i<10; ++i){
+        // blink for 10 times
+        ledpin = !ledpin;   // toggle Led
+        ThisThread::sleep_for(100ms);
+    }
+}
